182567158bd0a3ddee93dcc513620ec262b0c555
SY <ttzjwsupuabu@opayq.com>
2018-04-12 11:46:12 +0200

    Correction erreurs

--- diag.sh
+++ diag.sh
@@ -1,82 +1,108 @@
 #!/bin/sh
 
-# En attendant de pouvoir utiliser Git...
 # Created: Apr. 03, 2018
 # Author: Nabil LAMRABET
 
 # -------------------------- creation du fichier log ---------------------------
 
-dToday=`date -dtoday +%d-%m-%Y-%Hh%M`
-dFile=anomaly-diagnostic-file-$dToday.adf
+dateOfTheDay=`date -dtoday +%d-%m-%Y-%Hh%M`
+diagnosticFile=anomaly-diagnostic-file-$dateOfTheDay.adf
 tmp=tmp.adf
 
-touch dFile
+touch diagnosticFile
 touch tmp
 
-echo "-------------------------- Anomaly Diagnostic File : $dToday ------------\
+echo "-------------------------- Anomaly Diagnostic File : $dateOfTheDay ------------\
 --------------" >> dfile
 
 # -------------------------- detection des anomalies ---------------------------
 
 printf "Detection des anomalies, cela peut prendre quelques temps..."
 
-# on parcourt tous les dossiers clients
-# for i in `find -maxdepth 1 -type d`
-find . -maxdepth 1 -type d -print0 | while IFS= read -r -d $'\0' line;
+# On parcourt tous les dossiers clients.
+find . -maxdepth 1 -type d -print0 | while IFS= read -r -d $'\0' clientDirectory;
 do
-	printf "Verification du dossier client $i..."
-	# numberOfNonExpectedFolders= find -mindepth 1 -maxdepth 1 -type d -print |
-	#grep -v -E 'PAIES|LDM CLARTEO|Millesimes|^2[0-9]{3}(03|12)?$' | wc -l)
+	printf "Verification du dossier client $clientDirectory..."
 
-	# on écrit dans un fichier temporaire la liste des fichiers anormaux
-	# (se trouvant à la racine d'un dossier client)
-	find $line -mindepth 1 -maxdepth 1 -type f > $tmp
+	printf "Recherche de fichiers anormaux à la racine..."
 
+	# On écrit dans un fichier temporaire la liste des fichiers anormaux
+	# (se trouvant à la racine d'un dossier client).
+	find $clientDirectory -maxdepth 1 -type f >> $tmp
+
+	# On compte le nombre de fichiers anormaux,
+	# s'il y en a au moins un on affiche un message d'erreur dans notre fichier de diagnostique.
 	numberOfNonExpectedFiles=$(wc -l tmp.adf | cut -d" " -f1)
-	# on compte le nombre de fichiers, s'il y en a au moins un
 	if [ $numberOfNonExpectedFiles -gt 0 ]
 	then
-		echo "Des fichiers se trouvent à la racine du dossier client $line : ." >> $dFile
-		echo $tmp >> $dFile
+		printf "Des fichiers anormaux ont été trouvés."
+		printf "Ecriture des fichiers anormaux dans le fichier de diagnostique."
+		echo "Des fichiers se trouvent à la racine du dossier client $clientDirectory : ." >> $diagnosticFile
+		echo $tmp >> $diagnosticFile
 	fi
 
-	pathJ=`readlink -f $line`
-	directoryJ=$(basename $pathJ)
-	profondeur=$(echo $pathJ | grep -o "/" | wc -l) # profondeur
+	printf "Recherche de dossiers anormaux..."
+
+	clientDirectoryPath=`readlink -f $clientDirectory`
+	clientDirectoryName=`basename $clientDirectoryPath`
+	profondeur=$(echo $clientDirectoryPath | grep -o "/" | wc -l) # profondeur
 	profondeur=$(($profondeur+1))
 	racine=5 # a changer
 
-	for r in `find $file -mindepth 1 -maxdepth 1 -type d -print |
-	grep -v -E 'PAIES|LDM CLARTEO|^2[0-9]{3}(03|12)?$'`
+	for i in `find $clientDirectory -maxdepth 1 -type d -print grep -v -E 'PAIES|LDM CLARTEO|^2[0-9]{3}(03|12)?$'`
 	do
-		echo r >> fileDouble
-		find $r -type d -print | grep -v -E 'PAIES|LDM CLARTEO|^2[0-9]{3}(03|12)?$' >> fileDouble
+		echo $i >> abnormalDirectoriesPathFile
+		find $i -type d -print | grep -v -E 'PAIES|LDM CLARTEO|^2[0-9]{3}(03|12)?$' >> abnormalDirectoriesPathFile
 	done
 done
 
-numberofLineFileDouble=$(wc -l fileDouble | cut -d' ' -f1)
-# on parcourt notre fichier comportant la liste des dossiers anormaux
+printf "Traitement des données..."
+# abnormalDirectoriesPathFile est un fichier comportant la liste des chemins d'accès aux dossiers anormaux.
+# numberofAbnormalDirectories est le nombre de dossiers anormaux.
+# abnormalDirectoriesNameFile est la liste des dossiers anormaux sans leur chemin d'accès.
 
-for c in numberofLineFileDouble
+numberofAbnormalDirectories=$(wc -l abnormalDirectoriesPathFile | cut -d' ' -f1)
+# On parcourt notre fichier comportant la liste des dossiers anormaux.
+for c in numberofAbnormalDirectories
 do
-	# on extrait du path uniquement le nom des dossiers
-	# exemple :
+
+	# on extrait du path uniquement le nom des dossiers, exemple :
 	# /a/b/c -> c
-	basename `head -$c fileDouble  | tail -1` >> fileDouble2
+	basename `head -$c abnormalDirectoriesPathFile  | tail -1` >> abnormalDirectoriesNameFile
 done
 
-	find . -maxdepth 1 -type d -print0 | while IFS= read -r -d $'\0' line;
-	do
-		$count=0
-		for z in `cut -d' ' -f2 fileDouble2`
-		do
-			$count=$(($count+1))
-			if [ `basename $line` = $z ]
-			then
-				echo $line >> fileDouble
-				value=`head -$count fileDouble2  | tail -1 | cut -d' ' -f1`
-				sed -i 's/$value/$(($value+1))/' file.txt
-		done
-	done
+# Pour chaque dossier client,
 
-	sort fileCount2 | uniq -c > fileDouble2
+printf "recherche de dossiers multiples..."
+
+find . -maxdepth 1 -type d -print0 | while IFS= read -r -d $'\0' currentClientDirectory;
+do
+	$count=0
+
+	# et pour chaque dossier anormalement localisé,
+	for currentAnormalDirectory in `cat abnormalDirectoriesNameFile`
+	do
+		$count=$(($count+1))
+
+		# on compare si le dossier client et le dossier anormal portent le même nom,
+		if [ `basename $currentClientDirectory` = $currentAnormalDirectory ]
+		then
+			printf "Dossiers multiples trouvés."
+			printf "Ecriture des dossiers multiples dans le fichier de diagnostique."
+
+			# dans ce cas on ajoute le chemin d'accès du dossier clients
+			# à la liste des chemins d'accès de dossiers anormaux
+			echo $currentClientDirectory >> abnormalDirectoriesPathFile
+
+			# et on ajoute le nom du dossier à la liste des dossiers anormaux.
+			echo `basename $currentClientDirectory` >> abnormalDirectoriesNameFile
+
+			#	v1=`head -$count abnormalDirectoriesNameFile  | tail -1 | cut -d' ' -f1`
+			#	v2=$(($v1+1))
+			#	vtext=`head -$count abnormalDirectoriesNameFile  | tail -1 | cut -d' ' -f2`
+			#	sed -i '/$v1/ c\$v2 vtext' file.txt
+	done
+done
+
+	printf "Traitement des données..."
+	sort abnormalDirectoriesNameFile | uniq -c > multipleDirectoriesFile
